4fae974a380be99c25b471b380de483347351f51
Maxwell Daum <Maxwell.Daum@genesys.com>
2017-06-29 22:38:03 -0400

    believe I have random spot adding now...issue is these int IO to int casts...need to fix compilation errors

--- 2048.hs
+++ 2048.hs
@@ -19,14 +19,31 @@ printState board = do
 	putStrLn $ concat[(show line) ++ "\n"| line <- board]
 
 getEmptySpots :: [[Int]] -> [(Int,Int)]
-getEmptySpots board =
-	let indices = elemIndices 0 $ concat board
-	in [(floor((fromIntegral x)/4),mod x 4) | x <- indices]
+getEmptySpots board = [(floor((fromIntegral x)/4),mod x 4) | x <- (elemIndices 0 $ concat board)]
 
+addSpot :: [[Int]] -> [[Int]]
+addSpot board = do
+	let { empties = getEmptySpots board ;
+	spot = (empties !! (randomRIO(0,length empties-1))) ;
+	toAdd = ([2,2,2,2,2,2,2,2,2,4] !! (randomRIO(0,9))) }
+	return rewriteBoard board spot toAdd
+
+--will rewrite the board with added spot
+rewriteBoard :: [[Int]] -> (Int,Int) -> Int -> [[Int]]
+rewriteBoard board (row,col) toAdd = b4 ++ [target] ++ after
+	where { b4 = take row board ; --take everything b4 target row
+		  after = drop (row+1) ; --everything after target row
+		  target = take col (grid!!row) ++ [toAdd] ++ drop (col +1) (grid!!row) }
+			  
 gameLoop :: [[Int]] -> IO ()
 gameLoop board = do
-	printState board
-	
+	newBoard <- addSpot board
+	printState newBoard
+	continue <- getChar
+	gameLoop newBoard
+
+isFull :: [[Int]] -> Bool
+isFull board = notElem 0 (concat board)
 
 main :: IO ()
 main = do
