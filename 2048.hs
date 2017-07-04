23004bfa26a61275cdbce8cb59811cb8df6d7ca8
Maxwell Daum <Maxwell.Daum@genesys.com>
2017-07-04 15:59:07 -0400

    running into IO versus non-IO types again

--- 2048.hs
+++ 2048.hs
@@ -24,16 +24,24 @@ getEmptySpots board = [(floor((fromIntegral x)/4),mod x 4) | x <- (elemIndices 0
 addSpot :: [[Int]] -> [[Int]]
 addSpot board = rewriteBoard board spot toAdd
 	where { empties = getEmptySpots board ;
-	spotNum = 5 ;
+	spotNum = getRand (0, length empties-1) ;
 	spot = (empties !! spotNum) ;
 	addNum = 4 ;
 	toAdd = ([2,2,2,2,2,2,2,2,2,4] !! addNum) }
+	
+getRand :: (Int,Int) -> Int
+getRand range = toRet
+	where { 
+	g = getStdGen ;
+	toRet = fst $ randomR range g ;
+	trash = setStdGen(snd (next g)) }
+
 --will rewrite the board with added spot
 rewriteBoard :: [[Int]] -> (Int,Int) -> Int -> [[Int]]
 rewriteBoard board (row,col) toAdd = b4 ++ [target] ++ after
 	where { b4 = take row board ; --take everything b4 target row
 		  target = take col (board!!row) ++ [toAdd] ++ drop (col +1) (board!!row) ;
-		  after = drop (row+1) board }--everything after target row}
+		  after = drop (row+1) board }--everything after target row
 			  
 gameLoop :: [[Int]] -> IO ()
 gameLoop board = do
